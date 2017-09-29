328848c9249dae3c4e552bf949c13fcd06f119b8
hingbox <hingbox@163.com>
2017-09-29 13:54:42 +0800

    抓取

---
+++ test/cssandxpath/begin.py
@@ -0,0 +1,8 @@
+from scrapy import cmdline
+#cmdline.execute("scrapy crawl douban".split())
+#cmdline.execute("scrapy crawl test".split())
+#cmdline.execute("scrapy crawl news".split())
+#cmdline.execute("scrapy crawl huhu".split())
+#cmdline.execute("scrapy crawl chineseMedicine".split())
+#cmdline.execute("scrapy crawl test".split())
+cmdline.execute("scrapy crawl toscrape-css".split())
